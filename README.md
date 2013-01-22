d761af35b54a67a90b4ae556698229a9a3f999d8
abelykh <abelykh@corp.bluecherry.net>
2013-01-22 21:17:38 +0400

    --initial commit with folder structure and libs

--- README.md
+++
@@ -1,2 +0,0 @@
-bluecherry-web
-==============
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+    Copyright (c) 2010-2011 Bluecherry, LLC.
+    http://www.bluecherrydvr.com / support@bluecherrydvr.com
+    All rights reserved. No distribution is permitted.
+ */
+
+?>
